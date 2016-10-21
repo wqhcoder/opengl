0faeba5aa260882d3be98a76ca3818f057afa426
wuqianhu <wuqianhu@doodlemobile.com>
2016-10-21 16:07:45 +0800

    update

---
+++ assimp/build/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/E/profiles/opengl/assimp/assimp-3.1.1/contrib/zlib
+# Build directory: D:/E/profiles/opengl/assimp/build/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
